a4bba0387dba1e115c621b9b6e23bcf042fb2c01
Arnav Jha <overrideeducation@gmail.com>
2024-09-14 15:19:55 -0700

    Add Defense Code

---
+++ 2024-2025-High-Stakes/Templates/include/robot-config.h
@@ -0,0 +1,22 @@
+using namespace vex;
+
+extern brain Brain;
+
+// VEXcode devices
+extern motor LeftFront;
+extern motor LeftBack;
+extern motor RightFront;
+extern motor RightBack;
+extern motor Left6th;
+extern motor Right6th;
+extern inertial Inertial13;
+extern digital_out MogoPneu;
+extern digital_out Claw;
+extern motor Arm;
+extern motor Intake;
+/**
+ * Used to initialize code/tasks/devices added using tools in VEXcode Pro.
+ * 
+ * This should be called at the start of your int main function.
+ */
+void  vexcodeInit( void );
