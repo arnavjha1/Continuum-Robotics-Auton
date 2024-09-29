034e616ed813cd5166b9b6d9a27c9b63a8160434
Arnav Jha <overrideeducation@gmail.com>
2024-09-29 13:00:19 -0700

    Start of Elims Defensive

---
+++ 2024-2025-High-Stakes/Templates/ElimsDefense/src/robot-config.cpp
@@ -0,0 +1,26 @@
+#include "vex.h"
+
+using namespace vex;
+using signature = vision::signature;
+using code = vision::code;
+
+// A global instance of brain used for printing to the V5 Brain screen
+brain  Brain;
+
+// VEXcode device constructors
+motor LeftFront = motor(PORT14, ratio6_1, true);
+motor LeftBack = motor(PORT20, ratio6_1, true);
+motor RightFront = motor(PORT15, ratio6_1, false);
+motor RightBack = motor(PORT16, ratio6_1, false);
+motor Left6th = motor(PORT19, ratio6_1, true);
+motor Right6th = motor(PORT18, ratio6_1, false);
+inertial Inertial13 = inertial(PORT13);
+digital_out MogoPneu = digital_out(Brain.ThreeWirePort.B);
+digital_out Claw = digital_out(Brain.ThreeWirePort.A);
+motor Arm = motor(PORT9, ratio36_1, true);
+motor Intake = motor(PORT6, ratio6_1, false);
+limit LimitSwitchC = limit(Brain.ThreeWirePort.C);
+
+void vexcodeInit( void ) {
+  
+}
